255516b6d124cebc893b7d80dc023a60165ad052
Vincent5182 <46739285+Vincent5182@users.noreply.github.com>
2024-05-17 12:39:17 -0700

    movies update

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vada.Models;
 using Vada.ViewModels;
@@ -7,41 +9,51 @@ namespace Vada.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Batman" },
-                new Movie { Id = 2, Name = "Superman" }
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
         }
+
 
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
-            {
-                Name = "Iron Man"
-            };
+            var movie = new Movie() { Name = "Iron Man" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "John"},
-                new Customer {Name = "Jane"}
+                new Customer { Name = "John" },
+                new Customer { Name = "Jane" }
             };
 
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
-
             };
 
             return View(viewModel);
