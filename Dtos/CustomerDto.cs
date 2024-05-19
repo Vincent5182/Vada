656ef48871980a8d31535731cbd9d6fd98193265
Vincent5182 <46739285+Vincent5182@users.noreply.github.com>
2024-05-18 19:00:17 -0700

    added API

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vada.Models;
+
+namespace Vada.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter the customer name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribed { get; set; }
+        [Required(ErrorMessage = "Choose a membership type")]
+        public byte MembershipTypeId { get; set; }
+        [Adult]
+        public DateTime? Birthdate { get; set; }
+    }
+}
